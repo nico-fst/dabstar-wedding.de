f2f138d129c97ba92c70c028e21564e1c362be64
Nico Stern <nst.web@posteo.de>
2024-10-05 20:52:41 +0200

    (update.sh: + compose down)

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 echo "Pullin new GH version"
 git pull origin main
 
+echo "Stopping and removing old container"
+docker-compose down
+
 echo "Building new container"
 docker-compose build --no-cache
 docker-compose up -d
