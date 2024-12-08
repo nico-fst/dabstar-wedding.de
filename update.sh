19bf9c0d8d48ba8673df476d315974267e8f1e35
Nico (from Hetzner) <nst.web@posteo.de>
2024-12-08 02:35:01 +0100

    docker-compose -> docker compose

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ echo "Pullin new GH version"
 git pull origin main
 
 echo "Stopping and removing old container"
-docker-compose down
+docker compose down
 
 echo "Building new container"
-docker-compose build --no-cache
-docker-compose up -d
+docker compose build --no-cache
+docker compose up -d
