1bea9cf2ea6ec5d4f1b8ee5405555a9874b16e77
Nico Stern <nst.web@posteo.de>
2024-12-27 21:13:01 +0100

    (db -> wedsite_data

--- dabstar_wedsite/dabstar_wedsite/settings.py
+++ dabstar_wedsite/dabstar_wedsite/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "dabstar_wedsite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "wedsite_data" / "db.sqlite3",
     }
 }
 
